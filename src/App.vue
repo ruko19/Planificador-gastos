3a02b65c448edceef04744123cb785de3f3ccfd3
ruko19 <rukobaruna23@gmail.com>
2024-10-01 11:14:28 -0500

    formulario presupuesto

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 
 <script setup>
 
+import Presopuesto from './components/Presopuesto.vue';
+
 
 </script>
 
@@ -9,6 +11,13 @@
   <div>
     <header>
       <h1>Planificador de Gastos</h1>
+      <div class="contenedor contenedor-header sombra">
+
+        <Presopuesto
+  
+        />
+
+      </div>
     </header>
 
     
@@ -17,8 +26,64 @@
 </template>
 
 <style>
+  :root {
+    --azul: #3282f6;
+    --blanco: #fff;
+    --gris-claro: : #F5F5F5
+    --gris: #94a3b8;
+    --gris-oscuro: #64748b
+    --negro: #000;
+  }
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
 
+  }
 
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: "Lato", sans-serif;
+    background-color: var(--gris-claro);
+  }
+
+  h1 {
+    font-size: 4rem;
+  }
+  h2{
+    font-size: 3rem;
+  }
+header{
+  background-color: var(--azul);
+}
+
+header h1 {
+  padding: 3rem 0;
+  margin: 0;
+  color: var(--blanco);
+  text-align: center;
+}
+
+.contenedor {
+
+  width: 90%;
+  max-width: 80rem;
+  margin: 0 auto;
+
+}
+
+.contenedor-header {
+
+}
+
+.sombra {
+
+}
 
 
 
---
+++ src/components/Presopuesto.vue
@@ -0,0 +1,26 @@
+
+<script setup>
+
+</script>
+<template>
+    <form>
+        <div class="campo">
+            <label for="">Definir Presopuesto</label>
+            <input 
+                
+                id="nuevo-presopuesto "
+                class="nuevo-prosopuesto"
+                placeholder="Añade tu Presopuesto"
+            >
+
+            <input type="submit" value="Definir Presopuesto">
+
+
+        </div>
+
+    </form>
+</template>
+
+<style  scoped>
+
+</style>
